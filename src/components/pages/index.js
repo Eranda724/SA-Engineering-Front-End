d0414dccfa57aaadc1c6bcf0e2f8c26c96fe3974
Kaveesha Gimhan <kaveeshagimhan20@gmail.com>
2025-10-05 22:41:58 +0530

    Update index.js
    change to new Our team page

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,14 +1,25 @@
 // Page Components
-export { default as HomePage } from './HomePage';
-export { default as EngineeringServicesPage } from './EngineeringServicesPage';
-export { default as BoardOfDirectorsPage } from './OurTeamPage';
-export { default as BoardCommitteesPage } from './BoardCommitteesPage';
-export { default as ConstructionProjectsPage } from './ConstructionProjectsPage';
-export { default as InvestorsPage } from './InvestorsPage';
-export { default as NewsNotificationPage } from './NewsNotificationPage';
-export { default as OurStoryPage } from './OurStoryPage';
-export { default as OverviewPage } from './OverviewPage';
-export { default as PlaceholderPage } from './PlaceholderPage';
-export { default as PropertiesPage } from './PropertiesPage';
-export { default as SustainablePage } from './SustainablePage';
-export { default as OthersPage } from './OthersPage';
+export { default as HomePage }
+from './HomePage';
+export { default as EngineeringServicesPage }
+from './EngineeringServicesPage';
+export { default as OurTeamPage }
+from './OurTeamPage';
+export { default as ConstructionProjectsPage }
+from './ConstructionProjectsPage';
+export { default as InvestorsPage }
+from './InvestorsPage';
+export { default as NewsNotificationPage }
+from './NewsNotificationPage';
+export { default as OurStoryPage }
+from './OurStoryPage';
+export { default as OverviewPage }
+from './OverviewPage';
+export { default as PlaceholderPage }
+from './PlaceholderPage';
+export { default as PropertiesPage }
+from './PropertiesPage';
+export { default as SustainablePage }
+from './SustainablePage';
+export { default as OthersPage }
+from './OthersPage';
